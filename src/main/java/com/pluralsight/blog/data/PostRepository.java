525ee2f3f942d0bbce5c725e2d4267cdcc454f7f
dem20 <de_ni_se1@hotmail.com>
2020-10-27 22:39:54 -0600

    module 2 solved

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,10 +1,14 @@
 package com.pluralsight.blog.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.pluralsight.blog.model.Category;
 import com.pluralsight.blog.model.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long>{
 	
+	List<Post> findByCategory(Category category);
 }
 
